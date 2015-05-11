19e8c81f20df6a0f9abcf0ea7c7dace3ac733608
Jaccorot <caozijun007@163.com>
2015-05-11 20:47:27 +0800

    add the db operation

--- lottery/apps/website/spider.py
+++ lottery/apps/website/spider.py
@@ -2,14 +2,17 @@ import urllib2
 
 from bs4 import BeautifulSoup
 
+from lottery.apps.website.models import Num3D
+
 
 def catch_url(url):
-    this_turn = []
+    current_num = []
     url = 'http://cp.sogou.com/iframe/kaijiang/sohufragment/pl3/1.html'
     content = urllib2.urlopen(url)
     soup = BeautifulSoup(content, "html.parser")
-    turn = int(soup.select("option[selected]")[0].contents[0])
-    this_turn.append(turn)
+    current_turn = int(soup.select("option[selected]")[0].contents[0])
     for num_string in soup("i"):
-        this_turn.append(int(num_string.contents[0]))
-#    print this_turn
+        current_num.append(int(num_string.contents[0]))
+
+    db_temp = Num3D(turn=current_turn, num1=current_num[0], num2=current_num[1], num3=current_num[2])
+    db_temp.save()
